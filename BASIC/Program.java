bb8fe0ad6aea643846f70d12db59a4ead47d8664
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-03-02 07:17:56 -0500

    Update Program.java

--- BASIC/Program.java
+++ BASIC/Program.java
@@ -1,3 +1,9 @@
 
 public class Program {
+    public static int PROGRAM_START = 2;
+    public static int PROGRAM_RESTART = 1;
+    public static int PROGRAM_STOP = 0;
+    
+    public int system = PROGRAM_START;
+    
 }
