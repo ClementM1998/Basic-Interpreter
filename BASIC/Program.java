2ea76e3949256abd1de4734c842c4c0c4f782ee2
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-03-02 18:12:56 -0500

    Update Program.java

--- BASIC/Program.java
+++ BASIC/Program.java
@@ -6,4 +6,65 @@ public class Program {
     
     public int system = PROGRAM_START;
     
+    public static int PROGRESS_BAR_1 = 1; // versi 1 untuk animasi biasa
+    public static int PROGRESS_BAR_2 = 2; // versi 2 untuk progress bar biasa
+    public static int PROGRESS_BAR_3 = 3; // versi 3 untuk progress bar yang lebih interaktif
+    
+    private String getOSname() {
+        return System.getProperty("os.name");
+    }
+    
+    public void restart() {
+        clear();
+    }
+    
+    public void clear() {
+        try {
+            if (getOSname().toLowerCase().contains("win")) {
+                // Untuk Windows
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                // Untuk Linux dan Mac
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (Exception e) {
+            System.out.println("Gagal membersihkan skrin!");
+        }
+    }
+    
+    public void sleep(long times) {
+        try {
+            Thread.sleep(times);
+        } catch (Exception e) {}
+    }
+    
+    public void loading(int style, int total, long times) {
+        if (style == PROGRESS_BAR_1) {
+            String[] loadingFrames = {"|", "/", "-", "\\"};
+            System.out.print("Loading ");
+            for (int i = 0;i < total;i++) {
+                System.out.print("\rLoading " + loadingFrames[i % 4]);
+                sleep(times);
+            }
+            System.out.println("\rLoading Selesai!");
+        } else if (style == PROGRESS_BAR_2) {
+            System.out.print("Loading: [");
+            for (int i = 0;i <= total;i++) {
+                System.out.print("=");
+                sleep(times);
+            }
+            System.out.println("] 100% Selesai!");
+        } else if (style == PROGRESS_BAR_3) {
+            System.out.print("Loading: [");
+            for (int i = 0;i <= total;i++) {
+                System.out.print("=");
+                int percentage = (i * 100) / total;
+                System.out.print("] " + percentage + "%");
+                sleep(times);
+                System.out.print("\rLoading: [");
+            }
+            System.out.println("] 100% Selesai!");
+        }
+    }
+    
 }
