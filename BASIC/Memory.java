923c9a2c8a3115815a6fe041aeeaf172c3d8cac0
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-03-04 05:49:59 -0500

    Update Memory.java

--- BASIC/Memory.java
+++ BASIC/Memory.java
@@ -1,8 +1,60 @@
 
-public class Memory {
-    private int number;
-    private String line;
+import java.util.HashMap;
 
-    public Memory() {}
-    
+public class Memory {
+
+    private HashMap<Integer, String> memory;
+
+    public Memory() {
+        memory = new HashMap<Integer, String>();
+    }
+
+    public void add(int num, String line) {
+        if (memory == null) {
+            System.err.println("error: memory tidak boleh menyimpan");
+            return;
+        }
+        memory.put(num, line);
+    }
+
+    public String get(int num) {
+        if (memory == null) {
+            System.err.println("error: memory tidak boleh diambil");
+            return null;
+        }
+        return memory.get(num);
+    }
+
+    public boolean check(int num) {
+        if (memory == null) {
+            System.err.println("error: memory tidak boleh cek");
+            return false;
+        }
+        return memory.containsKey(num);
+    }
+
+    public void clear() {
+        if (memory == null) {
+            System.err.println("error: memory tidak boleh di bersihkan");
+            return;
+        }
+        memory.clear();
+    }
+
+    public boolean empty() {
+        if (memory == null) {
+            System.err.println("error: memory adalah null");
+            return false;
+        }
+        return memory.isEmpty();
+    }
+
+    public HashMap<Integer, String> getAll() {
+        if (memory == null) {
+            System.err.println("error: memory tidak boleh di akses");
+            return null;
+        }
+        return memory;
+    }
+
 }
