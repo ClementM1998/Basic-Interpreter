68ff6216cd7a00cbc0bffb5d8017200864c3e99f
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-03-07 22:15:24 -0800

    Create StructureBasic.java

---
+++ BASIC/StructureBasic.java
@@ -0,0 +1,24 @@
+
+public class StructureBasic {
+    /*
+     *
+     * +----------------------------------------------------+
+     * | 1. Input Handler                                  |
+     * |    (Menerima dan membaca arahan dari pengguna)    |
+     * +----------------------------------------------------+
+     * | 2. Lexer (Penganalisis Leksikal)                  |
+     * |    (Memecahkan arahan kepada token)               |
+     * +----------------------------------------------------+
+     * | 3. Parser (Penganalisis Sintaks)                  |
+     * |    (Memeriksa sintaks dan membentuk struktur AST) |
+     * +----------------------------------------------------+
+     * | 4. Executor (Pelaksana)                           |
+     * |    (Menjalankan kod baris demi baris)             |
+     * +----------------------------------------------------+
+     * | 5. Output Handler                                 |
+     * |    (Menghasilkan output kepada pengguna)         |
+     * +----------------------------------------------------+
+     *
+     *
+     */
+}
