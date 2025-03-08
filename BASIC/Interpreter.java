b20b38cbd9aa63929ba2347fde46664c5d4e0231
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-03-07 22:11:55 -0800

    Update Interpreter.java

--- BASIC/Interpreter.java
+++ BASIC/Interpreter.java
@@ -1,6 +1,15 @@
+import java.util.Map;
+import java.util.SortedMap;
 
 public class Interpreter {
-    
-    public Interpreter() {}
-    
+    private Lexer lexer;
+    private Parser parser;
+    private Executor executor;
+
+    public Interpreter(SortedMap<Integer, String> sorted) {
+
+    }
+
+    public void interpret() {}
+
 }
