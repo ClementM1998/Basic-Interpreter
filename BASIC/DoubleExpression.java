4696589edae9cec3ffc1f0418f1194d3ff8cf8cb
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-03-12 15:11:14 +0800

    Create DoubleExpression.java

---
+++ BASIC/DoubleExpression.java
@@ -0,0 +1,12 @@
+
+public class DoubleExpression extends Expression {
+    private final double value;
+
+    public DoubleExpression(double value) {
+        this.value = value;
+    }
+
+    public Expression evaluate(Environment env) {
+    }
+    
+}
