72a7fc821b01d3791849812bc2ff240b010bfe1f
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-03-12 15:19:51 +0800

    Update IntegerExpression.java

--- BASIC/IntegerExpression.java
+++ BASIC/IntegerExpression.java
@@ -1,3 +1,13 @@
 
 public class IntegerExpression extends Expression {
+    private final int value;
+
+    public IntegerExpression(int value) {
+        this.value = value;
+    }
+
+    public Expression evaluate(Environment env) {
+        return this;
+    }
+  
 }
