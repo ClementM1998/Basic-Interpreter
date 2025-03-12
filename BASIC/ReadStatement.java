0d49c7b9cea67f2d4d3d52db72297984abb69741
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-03-12 15:40:14 +0800

    Create ReadStatement.java

---
+++ BASIC/ReadStatement.java
@@ -0,0 +1,13 @@
+
+public class ReadStatement extends Statement {
+    private final String name;
+
+    public ReadStatement(String name) {
+        this.name = name;
+    }
+
+    public void execute(Environment env) {
+        
+    }
+    
+}
