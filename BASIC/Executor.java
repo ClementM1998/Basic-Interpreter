53f59d4277c2ad3e684f11a75a664176aa3e7c01
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-03-13 06:08:20 -0700

    Update Executor.java

--- BASIC/Executor.java
+++ BASIC/Executor.java
@@ -1,4 +1,27 @@
 
 public class Executor {
-  // Saya akan kemas kini tidak lama lagi.
+    private final Environment env;
+
+    public Executor(Environment env) {
+        this.env = env;
+    }
+
+    public void run() {
+        Integer currentLine = env.getNextStatement(); // Ambil baris pertama
+
+        while (currentLine != null) {
+
+            Statement stmt = env.getStatement(currentLine);
+
+            if (stmt == null) throw new RuntimeException("Nothing statement in line " + currentLine);
+
+            env.setCurrentLine(currentLine); // Tetapkan baris semasa
+            stmt.execute(env); // Jalankan statement
+
+            if (!env.isRunning()) break; // Berhenti jika program telah dihentikan
+
+            currentLine = env.getNextStatement(); // Ambil baris seterusnya
+
+        }
+    }
 }
