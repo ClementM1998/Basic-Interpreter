dde4a7b8413de5379304ab6c55f4f9d105225740
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-03-13 14:56:28 -0700

    Update PrintStatement.java

--- BASIC/PrintStatement.java
+++ BASIC/PrintStatement.java
@@ -6,8 +6,12 @@ public class PrintStatement extends Statement {
         this.expression = expression;
     }
 
-    public void execute(Enviroment env) {
-        Object value = expression.evaluate(env);
-        System.out.println(value);
+    public void execute(Environment env) {
+        Expression value = expression.evaluate(env);
+
+        if (value instanceof DoubleExpression) System.out.println(((DoubleExpression) value).value);
+        else if (value instanceof IntegerExpression) System.out.println(((IntegerExpression) value).value);
+        else System.out.println(((StringExpression) value).value);
     }
+
 }
