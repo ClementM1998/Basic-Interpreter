bfc344460494d737b0f7fcd5c96e2f48023f532e
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-03-13 06:09:18 -0700

    Update GosubStatement.java

--- BASIC/GosubStatement.java
+++ BASIC/GosubStatement.java
@@ -1,14 +1,13 @@
 
 public class GosubStatement extends Statement {
-    private final int lineNumber;
+    final int lineNumber;
 
     public GosubStatement(int lineNumber) {
         this.lineNumber = lineNumber;
     }
 
-    public void execute(Enviroment env) {
-        env.pushReturnAddress(env.getCurrentLine()); // Simpan baris semasa sebelum lompat
-        env.setLineNumber(lineNumber); // Lompat ke baris subrutin
+    public void execute(Environment env) {
+        env.callSubroutine(lineNumber);
     }
 
 }
