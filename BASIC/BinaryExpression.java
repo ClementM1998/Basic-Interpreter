803ffda81cb608a650cfc20dcae3e70c148d59c3
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-03-13 05:35:55 -0700

    Update BinaryExpression.java

--- BASIC/BinaryExpression.java
+++ BASIC/BinaryExpression.java
@@ -1,16 +1,11 @@
 
-public class BinaryExpression extends Expression {
+public abstract class BinaryExpression extends Expression {
     final Expression left;
-    final String operator;
     final Expression right;
 
-    public BinaryExpression(Expression left, String operator, Expression right) {
+    public BinaryExpression(Expression left, Expression right) {
         this.left = left;
-        this.operator = operator;
         this.right = right;
     }
-
-    public Expression evaluate(Enviroment env) {
-        return null;
-    }
+    
 }
