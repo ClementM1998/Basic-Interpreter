2b643d0789bb8c7743af83e9e6245cf47edb6183
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-03-13 15:01:25 -0700

    Update ReturnStatement.java

--- BASIC/ReturnStatement.java
+++ BASIC/ReturnStatement.java
@@ -2,8 +2,7 @@
 public class RetStatement extends Statement {
 
     public void execute(Enviroment env) {
-        int returnLine = env.popReturnAddress(); // Ambil baris yang disimpan dalam stack
-        env.setLineNumber(returnLine);
+        env.returnFromSubroutine();
     }
 
 }
