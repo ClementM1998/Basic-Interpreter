4fa7863b4ceebabbb67c4dec0b1fc812f3c336e1
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-03-13 06:07:09 -0700

    Create EndStatement.java

---
+++ BASIC/EndStatement.java
@@ -0,0 +1,10 @@
+
+public class EndStatement extends Statement {
+
+    public EndStatement() {
+    }
+
+    public void execute(Environment env) {
+        env.stopExecution(); // Hentikan program
+    }
+}
