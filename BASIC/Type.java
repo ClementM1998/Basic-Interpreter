c637af7ba909b3aa87782ae757b3361bf0fd377a
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-03-13 15:05:09 -0700

    Create Type.java

---
+++ BASIC/Type.java
@@ -0,0 +1,16 @@
+
+public enum Type {
+    READLINE,
+    NEWLINE,
+    LETTER,
+    KEYWORD, // PRINT LET IF THEN GOTO GOSUB
+    IDENTIFIER, // X Y VAR1
+    FUNCTION, // Fungsi math
+    NUMBER, // 123 3.14
+    STRING, // "HELLO"
+    OPERATOR, // + - * / = < > <= >= <>
+    SYMBOL, // ; ( ) [ ]
+    ERROR,
+    SEPARATOR, // (comma) digunakan dalam PRINT, READ, DATA
+    END, // Baris tamat
+}
