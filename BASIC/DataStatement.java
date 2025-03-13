b06dc3a17bbb88bc79e4c8e8de5fe2eec83cc5d2
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-03-13 05:55:20 -0700

    Update DataStatement.java

--- BASIC/DataStatement.java
+++ BASIC/DataStatement.java
@@ -1,14 +1,14 @@
 import java.util.ArrayList;
 
 public class DataStatement extends Statement {
-    private final ArrayList<Expression> data;
+    final ArrayList<Expression> data;
 
     public DataStatement(ArrayList<Expression> data) {
         this.data = data;
     }
 
     public void execute(Environment env) {
-        env.addData(data);
+        env.storeData(data);
     }
-    
+
 }
