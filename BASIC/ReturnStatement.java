9ea4680ede0a2a723a9c98130223e501b6655c37
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-03-31 20:39:43 -0700

    Update ReturnStatement.java

--- BASIC/ReturnStatement.java
+++ BASIC/ReturnStatement.java
@@ -1,8 +1,13 @@
 
 public class ReturnStatement extends Statement {
 
-    public void execute(Enviroment env) {
-        env.returnFromSubroutine();
+    public ReturnStatement() {
+    }
+
+    public void execute(Environment env) {
+        Integer line = env.popReturnAddress();
+        if (line == null) throw new RuntimeException("RETURN called without GOSUB");
+        env.setNextLine(line); // lompat ke baris asal
     }
 
 }
