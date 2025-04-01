8f492aa1c5a40264ae3df0ceb6f452ad532dd498
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-03-31 20:31:31 -0700

    Update InputStatement.java

--- BASIC/InputStatement.java
+++ BASIC/InputStatement.java
@@ -1,22 +1,35 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InputStatement extends Statement {
-    private final String name;
+    final ArrayList<String> varNames;
 
-    public InputStatement(String name) {
-        this.name = name;
+    public InputStatement(ArrayList<String> varNames) {
+        this.varNames = varNames;
     }
 
-    public void execute(Enviroment env) {
+    public void execute(Environment env) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("? "); // BASIC biasanya guna '?' untuk input
-        String input = scanner.nextLine();
-        try {
-            // Cuba tukar ke nombor jika boleh
-            if (input.contains(".")) env.assign(name, Double.parseDouble(input));
-            else env.assign(name, Integer.parseInt(input));
-        } catch (NumberFormatException e) {
-            env.assign(name, input); // Jika gagal, simpan sebagai string
+        for (String name : varNames) {
+            while (true) { // Loop sehingga input sah
+                System.out.print("? "); // BASIC biasanya guna '?' untuk input
+                String input = scanner.nextLine();
+                try {
+                    // Cuba tukar ke nombor jika boleh
+                    if (input.contains(".")) {
+                        if (env.hasVariable(name)) env.assignVariable(name, new DoubleExpression(Double.parseDouble(input)));
+                        else env.defineVariable(name, new DoubleExpression(Double.parseDouble(input)));
+                    } else {
+                        if (env.hasVariable(name)) env.assignVariable(name, new IntegerExpression(Integer.parseInt(input)));
+                        else env.defineVariable(name, new IntegerExpression(Integer.parseInt(input)));
+                    }
+                    break; // Keluar dari loop jika berjaya
+                } catch (NumberFormatException e) {
+                    //if (env.hasVariable(name)) env.assignVariable(name, new StringExpression(input)); // Jika gagal, simpan sebagai string
+                    //else env.defineVariable(name, new StringExpression(input));
+                    System.out.println("WHAT?"); // Sama BASIC Darthmouth
+                }
+            }
         }
     }
 }
